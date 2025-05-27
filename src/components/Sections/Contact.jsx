71c2ded92be862115cb004ae868637bac59b9363
Koushik-77 <koushikrachamalla3@gmail.com>
2025-05-27 20:17:28 +0530

    spelling mistake

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -38,7 +38,7 @@ export const Contact = () => {
                         required 
                         rows={5}
                         className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                        placeholder="Yout Message..."
+                        placeholder="Your Message..."
                         
                         ></textarea>
                         
